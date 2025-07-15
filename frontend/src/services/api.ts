d493567653a2badcb9bb0a116ab88dea1b7ce68c
Vatsalya Roy <79377087+Vatsalya001@users.noreply.github.com>
2025-07-15 21:18:14 +0530

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "https://deployed-booking-app-new-backend.onrender.com",
+  baseURL: "https://deployed-booking-app-new-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
